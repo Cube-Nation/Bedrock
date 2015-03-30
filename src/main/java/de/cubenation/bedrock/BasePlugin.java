92a68560015ed785dc0a5fac8d9dc2c03ac3e891
B1acksheep <B1acksheep@icloud.com>
2015-03-30 18:10:33 +0200

    Initial commit

---
+++ src/main/java/de/cubenation/bedrock/BasePlugin.java
@@ -0,0 +1,30 @@
+package de.cubenation.bedrock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by B1acksheep on 30.03.15.
+ * Project: Bedrock
+ * Package: de.cubenation.bedrock
+ */
+public class BasePlugin extends JavaPlugin {
+
+    public static BasePlugin instance;
+
+    public static BasePlugin getInstance() {
+        return BasePlugin.instance;
+    }
+
+    private void setInstance(JavaPlugin instance) {
+        BasePlugin.instance = (BasePlugin) instance;
+    }
+
+    @Override
+    public void onEnable() {
+        this.setInstance(this);
+
+        saveDefaultConfig();
+        reloadConfig();
+
+    }
+}
