73920489edcc013359a22668ddce165bf4345750
dirtyequi <equinox@lichtspiele.org>
2015-07-23 15:51:02 +0200

    added identifier

--- src/main/java/de/cubenation/bedrock/service/pageablelist/PageableListRegistry.java
+++ src/main/java/de/cubenation/bedrock/service/pageablelist/PageableListRegistry.java
@@ -1,17 +1,22 @@
 package de.cubenation.bedrock.service.pageablelist;
 
+import de.cubenation.bedrock.BasePlugin;
+import de.cubenation.bedrock.BedrockPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
 import java.util.HashMap;
 
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-
+@SuppressWarnings("unused")
 public class PageableListRegistry {
-	
+
+	private static int timeout;
+
 	/*
 	 * implicit synchronized singleton
 	 */
-	private static HashMap<CommandSender,PageableListService> registry
-		= new HashMap<CommandSender,PageableListService>();
+	private static HashMap<String,PageableListService> registry
+		= new HashMap<>();
 
 	private static final class InstanceHolder {
 		static final PageableListRegistry INSTANCE = new PageableListRegistry();
@@ -20,7 +25,9 @@ public class PageableListRegistry {
 	/*
 	 * private constructor
 	 */
-	private PageableListRegistry() {}
+	private PageableListRegistry() {
+        this.timeout = BedrockPlugin.getInstance().getConfig().getInt("service.pageablelist.timeout");
+    }
 	
 	/*
 	 * instance methods
@@ -29,19 +36,41 @@ public class PageableListRegistry {
 		return InstanceHolder.INSTANCE;
 	}
 
-	public void put(CommandSender sender, PageableListService service) {
-		registry.put(sender, service);
-	}
-	
-	public boolean has(CommandSender sender) {
-		return registry.containsKey(sender);
+    @SuppressWarnings("unused")
+	private String getRegistryIdentifier(BasePlugin plugin, String ident, CommandSender sender) {
+		return plugin.getName() + "|" + ident + "|" + sender.getName();
 	}
 
-	public PageableListService get(CommandSender sender) {
-		return registry.get(sender);
+    @SuppressWarnings("unused")
+	public void put(final BasePlugin plugin, final String ident, final CommandSender sender, PageableListService service, final int timeout) {
+		registry.put(this.getRegistryIdentifier(plugin, ident, sender), service);
+
+		Bukkit.getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
+            public void run() {
+                PageableListRegistry registry = PageableListRegistry.getInstance();
+                registry.remove(plugin, ident, sender);
+            }
+        }, 60L, (long) 20 * timeout);
 	}
 
-	public void remove(CommandSender sender) {
-		registry.remove(sender);
+    @SuppressWarnings("unused")
+	public void put(BasePlugin plugin, String ident, CommandSender sender, PageableListService service) {
+		this.put(plugin, ident, sender, service, this.timeout);
 	}
+
+    @SuppressWarnings("unused")
+	public boolean has(BasePlugin plugin, String ident, CommandSender sender) {
+		return registry.containsKey(this.getRegistryIdentifier(plugin, ident, sender));
+	}
+
+    @SuppressWarnings("unused")
+	public PageableListService get(BasePlugin plugin, String ident, CommandSender sender) {
+		return registry.get(this.getRegistryIdentifier(plugin, ident, sender));
+	}
+
+    @SuppressWarnings("unused")
+	public void remove(BasePlugin plugin, String ident, CommandSender sender) {
+		registry.remove(this.getRegistryIdentifier(plugin, ident, sender));
+	}
+
 }
