1dfef175758f243a8bf8a3026b04b93b1d78f0fd
dirtyequi <equinox@lichtspiele.org>
2015-07-23 15:49:17 +0200

    added generic storable for both services confirim and pageablelist

---
+++ src/main/java/de/cubenation/bedrock/helper/GenericStorable.java
@@ -0,0 +1,20 @@
+package de.cubenation.bedrock.helper;
+
+public class GenericStorable<T> {
+
+    private T object;
+
+    public GenericStorable() { }
+
+    public GenericStorable(T t) {
+        this.set(t);
+    }
+
+    public T get() {
+        return this.object;
+    }
+
+    public void set(T t) {
+        this.object = t;
+    }
+}
