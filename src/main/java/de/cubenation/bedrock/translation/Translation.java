c618fe75ea30675fdfcd2b65a6f13b1ba21ab355
dirtyequi <equinox@lichtspiele.org>
2015-07-24 20:23:12 +0200

    added Translation class

---
+++ src/main/java/de/cubenation/bedrock/translation/Translation.java
@@ -0,0 +1,52 @@
+package de.cubenation.bedrock.translation;
+
+import de.cubenation.bedrock.BasePlugin;
+import de.cubenation.bedrock.exception.LocalizationNotFoundException;
+import de.cubenation.bedrock.service.localization.LocalizationServiceInterface;
+
+public class Translation {
+
+    private final BasePlugin plugin;
+
+    private final LocalizationServiceInterface service;
+
+    private String locale_ident;
+
+    private String[] locale_args;
+
+    public Translation(BasePlugin plugin, String locale_ident) {
+        this(plugin, locale_ident, new String[]{});
+    }
+
+    public Translation(BasePlugin plugin, String locale_ident, String[] locale_args) {
+        this.plugin     = plugin;
+        this.service    = this.plugin.getLocalizationService();
+
+        this.setLocale_ident(locale_ident);
+        this.setLocale_args(locale_args);
+    }
+
+    public String getTranslation() {
+        try {
+            return this.service.getTranslation(this.getLocale_ident(), this.getLocale_args());
+        } catch (LocalizationNotFoundException e) {
+            return "";
+        }
+    }
+
+    private String getLocale_ident() {
+        return locale_ident;
+    }
+
+    private void setLocale_ident(String locale_ident) {
+        this.locale_ident = locale_ident;
+    }
+
+    private String[] getLocale_args() {
+        return locale_args;
+    }
+
+    private void setLocale_args(String[] locale_args) {
+        this.locale_args = locale_args;
+    }
+}
