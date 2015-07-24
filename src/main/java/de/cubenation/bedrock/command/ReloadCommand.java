b8e0b353626abd0afa6ecf9dfdac5b4a48cdf1bb
dirtyequi <equinox@lichtspiele.org>
2015-07-24 20:58:09 +0200

    added primitive reload command

---
+++ src/main/java/de/cubenation/bedrock/command/ReloadCommand.java
@@ -0,0 +1,11 @@
+package de.cubenation.bedrock.command;
+
+public abstract class ReloadCommand extends SubCommand {
+
+    public ReloadCommand(String command, String[] help, String permission) {
+        super(command, help, permission);
+
+        //FIXME: tbd
+    }
+
+}
