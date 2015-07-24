f8af5290fcabe33eb0c0ca8ae00809275fc2548d
dirtyequi <equinox@lichtspiele.org>
2015-07-24 21:25:24 +0200

    implementing registry interface, extending from abstract registry

--- src/main/java/de/cubenation/bedrock/service/pageablelist/PageableListRegistry.java
+++ src/main/java/de/cubenation/bedrock/service/pageablelist/PageableListRegistry.java
@@ -2,75 +2,60 @@ package de.cubenation.bedrock.service.pageablelist;
 
 import de.cubenation.bedrock.BasePlugin;
 import de.cubenation.bedrock.BedrockPlugin;
+import de.cubenation.bedrock.exception.NoSuchRegisterableException;
+import de.cubenation.bedrock.registry.AbstractRegistry;
+import de.cubenation.bedrock.registry.RegistryInterface;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
-import java.util.HashMap;
-
 @SuppressWarnings("unused")
-public class PageableListRegistry {
+public class PageableListRegistry extends AbstractRegistry implements RegistryInterface {
 
 	private static int timeout;
 
-	/*
-	 * implicit synchronized singleton
-	 */
-	private static HashMap<String,PageableListService> registry
-		= new HashMap<>();
+    /*
+     * make explicit singleton
+     */
+    private static final class InstanceHolder {
+        static final PageableListRegistry INSTANCE = new PageableListRegistry();
+    }
 
-	private static final class InstanceHolder {
-		static final PageableListRegistry INSTANCE = new PageableListRegistry();
-	}
-
-	/*
-	 * private constructor
-	 */
-	private PageableListRegistry() {
+    /*
+     * private constructor
+     */
+    private PageableListRegistry() {
         this.timeout = BedrockPlugin.getInstance().getConfig().getInt("service.pageablelist.timeout");
     }
-	
-	/*
-	 * instance methods
-	 */
-	public static PageableListRegistry getInstance () {
-		return InstanceHolder.INSTANCE;
-	}
 
-    @SuppressWarnings("unused")
-	private String getRegistryIdentifier(BasePlugin plugin, String ident, CommandSender sender) {
-		return plugin.getName() + "|" + ident + "|" + sender.getName();
-	}
+    /*
+     * instance methods
+     */
+    public static PageableListRegistry getInstance () {
+        return InstanceHolder.INSTANCE;
+    }
 
-    @SuppressWarnings("unused")
-	public void put(final BasePlugin plugin, final String ident, final CommandSender sender, PageableListService service, final int timeout) {
-		registry.put(this.getRegistryIdentifier(plugin, ident, sender), service);
 
-		Bukkit.getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
+    public static void register(BasePlugin plugin, String ident, CommandSender sender, PageableListServiceInterface object) {
+        getInstance()._register(plugin, ident, sender, object);
+
+        Bukkit.getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
             public void run() {
                 PageableListRegistry registry = PageableListRegistry.getInstance();
                 registry.remove(plugin, ident, sender);
             }
         }, 60L, (long) 20 * timeout);
-	}
+    }
 
-    @SuppressWarnings("unused")
-	public void put(BasePlugin plugin, String ident, CommandSender sender, PageableListService service) {
-		this.put(plugin, ident, sender, service, this.timeout);
-	}
+    public static boolean exists(BasePlugin plugin, String ident, CommandSender sender) {
+        return getInstance()._exists(plugin, ident, sender);
+    }
 
-    @SuppressWarnings("unused")
-	public boolean has(BasePlugin plugin, String ident, CommandSender sender) {
-		return registry.containsKey(this.getRegistryIdentifier(plugin, ident, sender));
-	}
+    public PageableListServiceInterface get(BasePlugin plugin, String ident, CommandSender sender) throws NoSuchRegisterableException {
+        return (PageableListServiceInterface) getInstance()._get(plugin, ident, sender);
+    }
 
-    @SuppressWarnings("unused")
-	public PageableListService get(BasePlugin plugin, String ident, CommandSender sender) {
-		return registry.get(this.getRegistryIdentifier(plugin, ident, sender));
-	}
-
-    @SuppressWarnings("unused")
-	public void remove(BasePlugin plugin, String ident, CommandSender sender) {
-		registry.remove(this.getRegistryIdentifier(plugin, ident, sender));
-	}
+    public void remove(BasePlugin plugin, String ident, CommandSender sender) {
+        getInstance()._remove(plugin, ident, sender);
+    }
 
 }
