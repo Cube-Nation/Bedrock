6028dd5167fee29ee98bc0efe21b693dc8699861
dirtyequi <equinox@lichtspiele.org>
2015-07-26 01:48:31 +0200

    added MessageHelper class

---
+++ src/main/java/de/cubenation/bedrock/helper/MessageHelper.java
@@ -0,0 +1,44 @@
+package de.cubenation.bedrock.helper;
+
+import de.cubenation.bedrock.BasePlugin;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public abstract class MessageHelper {
+
+    protected static BasePlugin plugin;
+
+    protected static ChatColor primary                = plugin.getPrimaryColor();
+
+    protected static ChatColor secondary              = plugin.getSecondaryColor();
+
+    protected static ChatColor flag                   = plugin.getFlagColor();
+
+    protected static void send(CommandSender sender, TextComponent message, HoverEvent hover_event, ClickEvent click_event) {
+        if (sender instanceof Player) {
+            if (hover_event != null)
+                message.setHoverEvent(hover_event);
+            if (click_event != null)
+                message.setClickEvent(click_event);
+
+            ((Player) sender).spigot().sendMessage(message);
+
+        } else {
+            String hover_message = "";
+
+            if (hover_event != null) {
+                hover_message = plugin.getFlagColor() + " (";
+                for (int i = 0; i < hover_event.getValue().length; i++) {
+                    hover_message += ChatColor.translateAlternateColorCodes('&', hover_event.getValue()[i].toLegacyText());
+                }
+                hover_message += plugin.getFlagColor() + ")";
+            }
+
+            sender.sendMessage(message.toPlainText() + hover_message);
+        }
+    }
+}
