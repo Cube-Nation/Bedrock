b4beab3fe28adba8fde4dbf2817cddb29023f03b
dirtyequi <equinox@lichtspiele.org>
2015-07-25 02:37:30 +0200

    logging / plugin gettser/setter

--- src/main/java/de/cubenation/bedrock/service/customconfigurationfile/CustomConfigurationFile.java
+++ src/main/java/de/cubenation/bedrock/service/customconfigurationfile/CustomConfigurationFile.java
@@ -1,16 +1,19 @@
 package de.cubenation.bedrock.service.customconfigurationfile;
 
+import de.cubenation.bedrock.BasePlugin;
 import de.cubenation.bedrock.registry.Registerable;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.logging.Level;
 
 public abstract class CustomConfigurationFile implements Registerable {
+
+	private BasePlugin plugin						= null;
 
 	private String filename							= null;
 	
@@ -20,11 +23,9 @@ public abstract class CustomConfigurationFile implements Registerable {
 	
 	private File file								= null;
 
-	private JavaPlugin plugin						= null;
-	
-	
-	public CustomConfigurationFile(JavaPlugin plugin, String filename, HashMap<String,Object> data) throws IOException {
-		this.plugin = plugin;
+
+	public CustomConfigurationFile(BasePlugin plugin, String filename, HashMap<String,Object> data) throws IOException {
+		this.setPlugin(plugin);
 		this.setFilename(filename);
 		
 		this.file = new File(
@@ -38,7 +39,16 @@ public abstract class CustomConfigurationFile implements Registerable {
 		
 		instance 	= this;
 	}
-	
+
+    public void setPlugin(BasePlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public BasePlugin getPlugin() {
+        return this.plugin;
+    }
+
+
 	private void setFilename(String filename) {
 		this.filename = filename;
 	}
@@ -47,16 +57,11 @@ public abstract class CustomConfigurationFile implements Registerable {
 		return filename;
 	}
 
-	public static CustomConfigurationFile getInstance() {
-		return instance;
-	}
-	
 	
 	@SuppressWarnings("rawtypes")
 	public void save(HashMap<String,Object> data) throws IOException {
-
-		this.plugin.getLogger().info(String.format(
-			"[Bedrock] Creating file %s for plugin %s",
+        this.getPlugin().log(Level.INFO, String.format(
+			"Creating file %s for plugin %s",
 			this.file.getAbsolutePath(),
 			this.plugin.getName()
 		));
@@ -72,19 +77,17 @@ public abstract class CustomConfigurationFile implements Registerable {
 
 		this.yaml.save(this.file.getAbsolutePath());		
 	}
-	
-	
+
 	public YamlConfiguration load() {
 		return YamlConfiguration.loadConfiguration(this.file);
 	}
-	
-	
+
 	public YamlConfiguration reload() {
-		this.plugin.getLogger().info(String.format(
-			"[Bedrock] Reloading file %s from plugin %s",
-			this.file.getAbsolutePath(),
-			this.plugin.getName()
-		));		
+		this.getPlugin().log(Level.INFO, String.format(
+                "[Bedrock] Reloading file %s from plugin %s",
+                this.file.getAbsolutePath(),
+                this.plugin.getName()
+        ));
 		return this.load();
 	}	
 
