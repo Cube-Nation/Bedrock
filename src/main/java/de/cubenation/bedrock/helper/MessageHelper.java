cba4008241869014a96b563181a704adcc796839
dirtyequi <equinox@lichtspiele.org>
2015-07-26 03:35:39 +0200

    added applyColors() method for preparation

--- src/main/java/de/cubenation/bedrock/helper/MessageHelper.java
+++ src/main/java/de/cubenation/bedrock/helper/MessageHelper.java
@@ -8,9 +8,20 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MessageHelper {
 
-    protected static void send(BasePlugin plugin, CommandSender sender, TextComponent message, HoverEvent hover_event, ClickEvent click_event) {
+    public static void send(BasePlugin plugin, CommandSender sender, String message) {
+        send(plugin, sender, new TextComponent(message));
+    }
+
+    public static void send(BasePlugin plugin, CommandSender sender, TextComponent component) {
+        send(plugin, sender, component, component.getHoverEvent(), component.getClickEvent());
+    }
+
+    public static void send(BasePlugin plugin, CommandSender sender, TextComponent message, HoverEvent hover_event, ClickEvent click_event) {
         if (sender instanceof Player) {
             if (hover_event != null)
                 message.setHoverEvent(hover_event);
@@ -32,5 +43,38 @@ public class MessageHelper {
 
             sender.sendMessage(message.toPlainText() + hover_message);
         }
+    }
+
+    @SuppressWarnings("unused")
+    private static String applyColors(BasePlugin plugin, String s) {
+        String regex =
+                "(&" +
+                        "(" +
+                        "BLACK|DARK_BLUE|DARK_GREEN|DARK_AQUA|DARK_RED|DARK_PURPLE|GOLD|GRAY|DARK_GRAY|BLUE|GREEN|AQUA|RED|LIGHT_PURPLE|YELLOW|WHITE" +
+                        "|" +
+                        "STRIKETHROUGH|UNDERLINE|BOLD|MAGIC|ITALIC|RESET" +
+                        "|" +
+                        "PRIMARY|SECONDARY|FLAG" +
+                        ")" +
+                "&)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        StringBuffer sb = new StringBuffer();
+
+        while (matcher.find()) {
+            if (matcher.group(2).equals("PRIMARY")) {
+                matcher.appendReplacement(sb, "" + plugin.getPrimaryColor());
+            } else if (matcher.group(2).equals("SECONDARY")) {
+                matcher.appendReplacement(sb, "" + plugin.getSecondaryColor());
+            } else if (matcher.group(2).equals("FLAG")) {
+                matcher.appendReplacement(sb, "" + plugin.getFlagColor());
+            } else {
+                matcher.appendReplacement(sb, ChatColor.valueOf(matcher.group(2)).toString());
+            }
+        }
+
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 }
