e6d694415c3b0f4a83610b27e30dedb53d23a718
B1acksheep <B1acksheep@icloud.com>
2015-07-29 02:31:39 +0200

    Change default color scheme

--- src/main/java/de/cubenation/bedrock/style/scheme/DefaultColorScheme.java
+++ src/main/java/de/cubenation/bedrock/style/scheme/DefaultColorScheme.java
@@ -8,7 +8,7 @@ import net.md_5.bungee.api.ChatColor;
 public class DefaultColorScheme extends ColorScheme {
 
     public DefaultColorScheme(BasePlugin plugin) {
-        super(ColorSchemeName.DEFAULT, ChatColor.AQUA, ChatColor.WHITE, ChatColor.GRAY, ChatColor.WHITE);
+        super(ColorSchemeName.DEFAULT, ChatColor.BLUE, ChatColor.AQUA, ChatColor.GRAY, ChatColor.WHITE);
     }
 
 }
