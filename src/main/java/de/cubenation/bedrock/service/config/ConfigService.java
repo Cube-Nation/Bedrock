997b1c8544bcec1d780950edb1dca69dc20d6795
dirtyequi <equinox@lichtspiele.org>
2015-08-01 20:52:35 +0200

    The "Slippery When Wet" Update - pre-alpha

--- src/main/java/de/cubenation/bedrock/service/config/ConfigService.java
+++ src/main/java/de/cubenation/bedrock/service/config/ConfigService.java
@@ -12,13 +12,14 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 
 public class ConfigService extends AbstractService implements ServiceInterface {
 
     private HashMap<String, CustomConfigurationFile> configuration_files = new HashMap<>();
+
+    private final String do_not_delete_me = "Seriously. Do not delete this";
 
     public ConfigService(BasePlugin plugin) {
         super(plugin);
@@ -30,25 +31,14 @@ public class ConfigService extends AbstractService implements ServiceInterface {
         this.createDataFolder();
 
         // try to create the plugin config.yml
-        CustomConfigurationFile config;
         try {
-            this.registerFile("config.yml", this.createPluginConfig(
-                    this.getPlugin(),
-                    String.format("%s.config.BedrockDefaults",
-                            this.getPlugin().getClass().getPackage().getName()
-                    ),
-                    "config.yml"
-            ));
-        } catch (InstantiationException ignored) {
+            this.registerFile("config.yml", this.instantiatePluginConfig());
+        } catch (InstantiationException | InvalidConfigurationException ignored) {
 
             // try to create the config.yml from Bedrock (yes, for this plugin)
             try {
-                this.registerFile("config.yml", this.createPluginConfig(
-                        this.getPlugin(),
-                        BedrockDefaults.class.getName(),
-                        "config.yml"
-                ));
-            } catch (InstantiationException e) {
+                this.registerFile("config.yml", this.instantiateBedrockConfig());
+            } catch (InstantiationException | InvalidConfigurationException e) {
                 throw new ServiceInitException(e.getMessage());
             }
         }
@@ -60,16 +50,14 @@ public class ConfigService extends AbstractService implements ServiceInterface {
          * now try reading the service.config.do_not_delete_me value. If it does not exist, is empty or has been changed
          * ths default config file will be overwritten merciless *muhaha*
          */
-        /*
         if (!this.isValidPluginConfiguration()) {
             this.getPlugin().log(Level.WARNING, "The plugin configuration file config.yml became invalid and will be recreated from scratch");
             try {
-                this.writeDefaultBedrockConfig(true);
-            } catch (IOException e) {
+                this.registerFile("config.yml", this.instantiatePluginConfig());
+            } catch (InstantiationException | InvalidConfigurationException e) {
                 throw new ServiceInitException(e.getMessage());
             }
         }
-        */
     }
 
     @Override
@@ -102,9 +90,31 @@ public class ConfigService extends AbstractService implements ServiceInterface {
     }
 
 
+    private CustomConfigurationFile instantiatePluginConfig() throws InstantiationException, InvalidConfigurationException {
+        CustomConfigurationFile config = this.createPluginConfig(
+                this.getPlugin(),
+                String.format("%s.config.BedrockDefaults",
+                        this.getPlugin().getClass().getPackage().getName()
+                ),
+                "config.yml"
+        );
+        config.init();
+        return config;
+    }
+
+    private CustomConfigurationFile instantiateBedrockConfig() throws InstantiationException, InvalidConfigurationException {
+        CustomConfigurationFile config = this.createPluginConfig(
+                this.getPlugin(),
+                BedrockDefaults.class.getName(),
+                "config.yml"
+        );
+        config.init();
+        return config;
+    }
+
     private CustomConfigurationFile createPluginConfig(BasePlugin plugin, String class_name, String name) throws InstantiationException {
         try {
-            Class<?> clazz = (Class<?>) Class.forName(class_name);
+            Class<?> clazz = Class.forName(class_name);
             Constructor<?> constructor = clazz.getConstructor(BasePlugin.class, String.class);
             return (CustomConfigurationFile) constructor.newInstance(plugin, name);
         } catch (Exception e) {
@@ -116,9 +126,8 @@ public class ConfigService extends AbstractService implements ServiceInterface {
         if (custom_configuration_files == null || custom_configuration_files.size() == 0)
             return;
 
-        Iterator it = custom_configuration_files.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
+        for (Object o : custom_configuration_files.entrySet()) {
+            Map.Entry pair = (Map.Entry) o;
 
             try {
                 this.registerFile((String) pair.getKey(), this.createPluginConfig(
@@ -157,6 +166,7 @@ public class ConfigService extends AbstractService implements ServiceInterface {
     }
 
     @Deprecated
+    @SuppressWarnings("deprecation")
     public YamlConfiguration getReadOnlyConfig() {
         return getReadOnlyConfig("config.yml");
     }
@@ -166,121 +176,13 @@ public class ConfigService extends AbstractService implements ServiceInterface {
         File file = new File(this.getPlugin().getDataFolder().getAbsolutePath() + System.getProperty("file.separator") + name);
         return (file.exists()) ? YamlConfiguration.loadConfiguration(file) : null;
     }
-
-
-    public void saveConfig(String name) throws InvalidConfigurationException {
-        if (name == null || name.isEmpty())
-            return;
-
-        CustomConfigurationFile file = this.configuration_files.get(name);
-        if (file == null)
-            return;
-
-        this.getPlugin().log(Level.INFO, "  config service: Saving configuration file " + name);
-        file.save();
-        file.reload();
-    }
-
-    /*
-    private void writeDefaultBedrockConfig(boolean overwrite) throws IOException {
-
-        String filename = System.getProperty("file.separator") + "config.yml";
-        File plugin_config  = new File(this.getPlugin().getDataFolder().getAbsolutePath() + filename);
-        File bedrock_config = new File(BedrockPlugin.getInstance().getDataFolder().getAbsolutePath() + filename);
-
-        // Bedrock Plugin is calling
-        if (this.getPlugin() instanceof BedrockPlugin) {
-            if (!bedrock_config.exists() || overwrite) {
-                this.getPlugin().log(Level.INFO, "Creating default configuration from scratch for BedrockPlugin");
-                this.getPlugin().saveDefaultConfig();
-            }
-            return;
-        }
-
-        if (!plugin_config.exists() || overwrite) {
-            this.getPlugin().log(Level.INFO, "Creating default configuration from scratch for " + this.getPlugin().getDescription().getName());
-            Files.copy(
-                    bedrock_config.toPath(),
-                    plugin_config.toPath(),
-                    StandardCopyOption.REPLACE_EXISTING,
-                    StandardCopyOption.COPY_ATTRIBUTES,
-                    LinkOption.NOFOLLOW_LINKS
-            );
-
-            this.extendDefaultPluginConfig();
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    private void extendDefaultPluginConfig() {
-
-        InputStream config_stream = this.getPlugin().getResource("bedrock_defaults.yml");
-
-        // nothing to extend
-        if (config_stream == null)
-            return;
-
-
-
-        final YamlConfiguration plugin_config;
-        if (this.getPlugin().getDescription().getAwareness().contains(PluginAwareness.Flags.UTF8) || FileConfiguration.UTF8_OVERRIDE) {
-            plugin_config = YamlConfiguration.loadConfiguration(new InputStreamReader(config_stream, Charsets.UTF_8));
-        } else {
-            final byte[] contents;
-            plugin_config = new YamlConfiguration();
-            try {
-                contents = ByteStreams.toByteArray(config_stream);
-            } catch (final IOException e) {
-                this.getPlugin().log(Level.SEVERE, "Unexpected failure reading bedrock_defaults.yml", e);
-                return;
-            }
-
-            final String text = new String(contents, Charset.defaultCharset());
-            if (!text.equals(new String(contents, Charsets.UTF_8)))
-                this.getPlugin().log(Level.WARNING,
-                        "Default system encoding may have misread config.yml from plugin jar"
-                );
-
-            try {
-                plugin_config.loadFromString(text);
-            } catch (final InvalidConfigurationException e) {
-                this.getPlugin().log(Level.WARNING,
-                        "Cannot load configuration from plugin jar. Cannot extend default configuration", e
-                );
-            }
-        }
-
-        HashMap<String,Object> plugin_config_settings = resolveDefaultPluginConfig(plugin_config);
-        for (String key : plugin_config_settings.keySet()) {
-            this.getPlugin().log(Level.INFO, String.format("Setting default value of %s to %s",
-                    key, plugin_config_settings.get(key).toString()
-            ));
-            this.getPlugin().getConfig().set(key, plugin_config_settings.get(key));
-        }
-        this.getPlugin().getConfig().options().copyDefaults(true);
-        this.getPlugin().saveConfig();
-    }
-
-    private HashMap<String,Object> resolveDefaultPluginConfig(YamlConfiguration config) {
-        HashMap<String,Object> data = new HashMap<>();
-        for (String key : config.getKeys(true)) {
-            try {
-                if (config.getConfigurationSection(key).getKeys(false).size() != 0)
-                    continue;
-            } catch (NullPointerException ignored) {
-            }
-
-            data.put(key, config.get(key));
-        }
-        return data;
-    }
-
+    
     private boolean isValidPluginConfiguration() {
         try {
-            return this.getPluginConfig().getString("service.config.do_not_delete_me").equals("Seriously. Do not delete this");
+            return (this.getConfigurationValue("service.config.do_not_delete_me", null).equals(this.do_not_delete_me));
         } catch (NullPointerException e) {
             return false;
         }
     }
-*/
+
 }
