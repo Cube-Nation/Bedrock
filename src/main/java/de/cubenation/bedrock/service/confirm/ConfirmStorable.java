d07bda765f86409dc4b79265fc1cc6150d7f8861
dirtyequi <equinox@lichtspiele.org>
2015-09-05 11:39:23 +0200

    added second constructor with argument

--- src/main/java/de/cubenation/bedrock/service/confirm/ConfirmStorable.java
+++ src/main/java/de/cubenation/bedrock/service/confirm/ConfirmStorable.java
@@ -2,4 +2,11 @@ package de.cubenation.bedrock.service.confirm;
 
 import de.cubenation.bedrock.helper.GenericStorable;
 
-public class ConfirmStorable<T> extends GenericStorable { }
+public class ConfirmStorable<T> extends GenericStorable {
+
+    @SuppressWarnings("unchecked")
+    public ConfirmStorable(T t) {
+        this.set(t);
+    }
+
+}
