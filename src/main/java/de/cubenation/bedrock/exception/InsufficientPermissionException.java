e0a5af4378df88511c9dcc420f5ce07091f691b0
dirtyequi <equinox@lichtspiele.org>
2015-10-14 20:24:08 +0200

    initial import

---
+++ src/main/java/de/cubenation/bedrock/exception/InsufficientPermissionException.java
@@ -0,0 +1,9 @@
+package de.cubenation.bedrock.exception;
+
+public class InsufficientPermissionException extends Exception {
+
+    public InsufficientPermissionException(String message) {
+        super(message);
+    }
+
+}
