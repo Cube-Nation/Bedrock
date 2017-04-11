a701076a8c9cda6ed00ff52d3eae9e9e69a31202
Cebulla, Tristan <tristan.cebulla@1und1.de>
2017-04-11 16:22:46 +0200

    default condition that allows by default

---
+++ src/main/java/de/cubenation/api/bedrock/annotation/condition/DefaultCondition.java
@@ -0,0 +1,9 @@
+package de.cubenation.api.bedrock.annotation.condition;
+
+public class DefaultCondition extends AnnotationCondition {
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+}
