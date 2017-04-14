bd76aa3ac37c422f13a42cf6121a09d2a90be3a4
Tristan Cebulla <equinox@lichtspiele.org>
2017-04-14 21:24:20 +0200

    adds UNUSED role and some doc

--- src/main/java/de/cubenation/api/bedrock/command/CommandRole.java
+++ src/main/java/de/cubenation/api/bedrock/command/CommandRole.java
@@ -7,12 +7,28 @@ package de.cubenation.api.bedrock.command;
 
 @SuppressWarnings("unused")
 public enum CommandRole {
+    // all permissions with no role are in this role by default
     NO_ROLE("no_role"),
+
+    // assign permissions to this role if you don't want to use them
+    UNUSED("unused"),
+
+    // guest users
     GUEST("guest"),
+
+    // default users
     USER("user"),
+
+    // more privileged users
     SUPPORTER("supporter"),
+
+    // much more privileged users
     MODERATOR("moderator"),
+
+    // administrators
     ADMIN("admin"),
+
+    // root
     OWNER("owner");
 
     private final String type;
