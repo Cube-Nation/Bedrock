94a117a9f6c3187d70388b29ecd2ddb3c1770493
Tristan Cebulla <equinox@lichtspiele.org>
2017-04-17 20:24:08 +0200

    adds exception in case a service already exists

---
+++ src/main/java/de/cubenation/api/bedrock/exception/ServiceAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package de.cubenation.api.bedrock.exception;
+
+public class ServiceAlreadyExistsException extends Exception {
+
+    public ServiceAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
